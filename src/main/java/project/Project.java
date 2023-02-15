df880973014c86b12cc2092d5f0a3806eef3a8b2
Mike <Mikejansen2@icloud.com>
2023-02-15 16:32:34 +0100

    added project and all functionalities

--- src/main/java/project/Project.java
+++ src/main/java/project/Project.java
@@ -1,4 +1,41 @@
 package main.java.project;
 
+import main.java.user.ProductOwnerUser;
+
 public class Project {
+    ProductBacklog productBacklog;
+    ProductOwnerUser productOwner;
+    String projectName;
+
+    public Project(String name, ProductOwnerUser productOwner){
+        this.projectName = name;
+        this.productOwner = productOwner;
+    }
+
+    public ProductBacklog getProductBacklog(){
+        return this.productBacklog;
+    }
+
+    public String addProductBacklog(ProductBacklog productBacklog){
+        this.productBacklog = productBacklog;
+
+        if(this.productBacklog != productBacklog){
+            return "Product backlog not added. Try again!";
+        }
+
+        return "Product backlog added!";
+    }
+
+    public ProductOwnerUser getProductOwner(){
+        return this.productOwner;
+    }
+
+    public String getProjectName(){
+        return this.projectName;
+    }
+
+    public String setProjectName(String name){
+        this.projectName = name;
+        return "Project name set to: " + this.projectName;
+    }
 }
