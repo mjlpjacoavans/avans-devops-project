86cc9b6c1f072a20ae8d8f957cb043a0aca324c9
mjlpjacoavans <mjlpjaco@avans.nl>
2023-03-02 14:59:31 +0100

    feedback commits

--- src/main/java/pipeline/DevelopmentPipeline.java
+++ src/main/java/pipeline/DevelopmentPipeline.java
@@ -7,4 +7,12 @@ public class DevelopmentPipeline extends BasePipeline{
     public DevelopmentPipeline() {
         super();
     }
+
+    // TODO: Add other override constraints
+
+    @Override
+    final public Boolean runDeploy() {
+        System.out.println("Deploy can not be ran in development pipeline. Skipping.");
+        return true;
+    }
 }
