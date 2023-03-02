ecbd774267b474d1c15d15952b656120e5bdb6b9
mjlpjacoavans <mjlpjaco@avans.nl>
2023-03-02 15:03:41 +0100

    new todo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+//        TODO: Add adapter pattern somewhere
 //        //Create a product owner
 //        ProductOwnerUser productOwner1 = new ProductOwnerUser("Gerard", "Gerard@email.com");
 //
