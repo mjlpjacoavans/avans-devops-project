bd9615abf21664864e8da34f247d7992a8875b68
mjlpjacoavans <mjlpjaco@avans.nl>
2023-03-13 13:26:54 +0100

    strange bug

--- src/main/java/notification/NotificationBehaviourFactory.java
+++ src/main/java/notification/NotificationBehaviourFactory.java
@@ -1,6 +1,8 @@
 package notification;
 
 
+import main.java.notification.behaviours.*;
+
 // TODO: Replace all DynamicNotificationBehaviour with this factory
 public class NotificationBehaviourFactory {
     INotificationBehaviour create(){
@@ -8,11 +10,11 @@ public class NotificationBehaviourFactory {
         return this.create(notificationBehaviourType);
     }
     INotificationBehaviour create(NotificationBehaviourTypes notificationBehaviourType){
-        NotificationBehaviourTypes retNotificationBehaviourType = ( notificationBehaviourType == NotificationBehaviourTypes.EMAIL)  ?  new EmailNotificaionBehaviour()
+        INotificationBehaviour retNotificationBehaviourType =
+                  ( notificationBehaviourType == NotificationBehaviourTypes.EMAIL)  ?  new EmailNotificaionBehaviour()
                 : ( notificationBehaviourType == NotificationBehaviourTypes.SLACK)  ?  new SlackNotificaionBehaviour()
                 : ( notificationBehaviourType == NotificationBehaviourTypes.STDOUT) ?  new StdOutNotificationBehaviour()
                 : null;
-
         if(retNotificationBehaviourType == null) throw new IllegalArgumentException("Notification behaviour type not supported")
         return retNotificationBehaviourType;
     }
