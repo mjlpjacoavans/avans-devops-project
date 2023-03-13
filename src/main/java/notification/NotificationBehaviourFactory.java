83615e96aea4036e70c547985d895f70b7debdb2
mjlpjacoavans <mjlpjaco@avans.nl>
2023-03-13 13:19:49 +0100

    created a propper factory instead of the strange DynamicNotificationBehaviour

---
+++ src/main/java/notification/NotificationBehaviourFactory.java
@@ -0,0 +1,19 @@
+package notification;
+
+
+// TODO: Replace all DynamicNotificationBehaviour with this factory
+public class NotificationBehaviourFactory {
+    INotificationBehaviour create(){
+        NotificationBehaviourTypes notificationBehaviourType = NotificationBehaviourTypes.EMAIL;
+        return this.create(notificationBehaviourType);
+    }
+    INotificationBehaviour create(NotificationBehaviourTypes notificationBehaviourType){
+        NotificationBehaviourTypes retNotificationBehaviourType = ( notificationBehaviourType == NotificationBehaviourTypes.EMAIL)  ?  new EmailNotificaionBehaviour()
+                : ( notificationBehaviourType == NotificationBehaviourTypes.SLACK)  ?  new SlackNotificaionBehaviour()
+                : ( notificationBehaviourType == NotificationBehaviourTypes.STDOUT) ?  new StdOutNotificationBehaviour()
+                : null;
+
+        if(retNotificationBehaviourType == null) throw new IllegalArgumentException("Notification behaviour type not supported")
+        return retNotificationBehaviourType;
+    }
+}
